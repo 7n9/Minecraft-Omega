6c547017b43b8ce0940a94fcca9224774aa64def
Owen2k6 <fulptdm@gmail.com>
2023-08-31 17:50:08 +0100

    o

--- src/minecraft/com/owen2k6/omega/recipies/Recipes.java
+++ src/minecraft/com/owen2k6/omega/recipies/Recipes.java
@@ -1,11 +1,10 @@
 package com.owen2k6.omega.recipies;
 
-import net.minecraft.src.Block;
 import net.minecraft.src.CraftingManager;
 import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
 
-public class crackMagic {
+public class Recipes {
     public void addRecipes(CraftingManager craftingManager1) {
         craftingManager1.addRecipe(new ItemStack(Item.magicDust, 1), "#",
                 '#', Item.magicreed);
